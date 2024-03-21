e97373aba670de272c85ce6eecb3c18859182f77
MauricioPassosF <mauricio.passos97@gmail.com>
2024-03-21 18:52:46 -0300

    Criando rotas e linter

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -2,11 +2,18 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    project:'tsconfig.eslint.json',
+    tsconfigRootDir: './app',
+    sourceType: 'module' 
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs',"**/setupTests.ts", "**/vite.config.ts"],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
--- app/src/page/Home.tsx
+++ app/src/page/Home.tsx
@@ -1,5 +1,12 @@
-const Home = () => {
-    return <div>Home</div>
+import React from 'react';
+
+function Home(): JSX.Element {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h1>World</h1>
+    </div>
+  );
 }
 
 export default Home;
