2f171284cf5b44734c84a02a0b89645eb71bbf25
André Lucas <andrelucca99@gmail.com>
2024-03-22 16:30:45 -0300

    refactor: update directories the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-green-500 w-full h-screen">
+      <h1 className="text-center text-3xl text-white font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
