d152dfa4feba83559e19cc966627a0216fc888b5
unknown <vitor_teixeira90@hotmail.com>
2024-03-26 14:32:24 -0300

    feat: header e componentização do card

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../public/1601633424-47922400748457936cb2b276d0c36c4682f131441c3cbb808e9fa91730160712-d_640.webp";
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+  const [nav, setNav] = useState<boolean>(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div>
+      <div className="fixed w-full shadow-xl h-20 z-[100] bg-blue-100">
+        <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+          <Link href="/">
+            <Image src={logo} alt="Logo" width="50" height="5" />
+          </Link>
+          <div>
+            <ul className="hidden md:flex">
+              <Link href="/">
+                <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+              </Link>
+
+              <Link href="/">
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  Projetos
+                </li>
+              </Link>
+
+              <Link href="/">
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  Sobre
+                </li>
+              </Link>
+
+              <Link href="/">
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  Contatos
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <div onClick={handleNav} className="md:hidden cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div
+          className={
+            nav
+              ? " md:hidden fixed left-0 top-0 w-full h-screen bg-black/70"
+              : ""
+          }
+        >
+          <div
+            className={
+              nav
+                ? "fixed left-0 top-0 w-[75%] sm:w[60%] md:w[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+                : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+            }
+          >
+            <div className="flex justify-between w-full items-center">
+              <Image src={logo} alt="Logo" width="80" height="35" />
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">
+                criando o melhor projeto para você
+              </p>
+            </div>
+            <div className="py-4 flex flex-col">
+              <ul>
+                <Link href="/">
+                  <li className="text-sm uppercase hover:border-b py-4">
+                    Home
+                  </li>
+                </Link>
+
+                <Link href="/projetos">
+                  <li className="text-sm uppercase hover:border-b py-4">
+                    Projetos
+                  </li>
+                </Link>
+
+                <Link href="/sobre">
+                  <li className="text-sm uppercase hover:border-b py-4">
+                    Sobre
+                  </li>
+                </Link>
+
+                <Link href="/contato">
+                  <li className="text-sm uppercase hover:border-b py-4">
+                    Contatos
+                  </li>
+                </Link>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
