14d3148539990b0d0a52ecfccea6ebbb88b15171
MauricioPassosF <mauricio.passos97@gmail.com>
2024-03-27 12:56:37 -0300

    Estilização inicial e filtro alterando estado

--- app/src/components/Search.tsx
+++ app/src/components/Search.tsx
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/Search.css';
+import { Filter } from '../types/FilterType';
 
 function Search(): JSX.Element {
-  // const [filter, setFilter] = useState({
-  //   tipoDestaque: '',
-  //   stacks: '',
-  // });
+  const [filter, setFilter] = useState<Filter>({
+    tipoDestaque: '',
+    stacks: '',
+  });
+
+  const handleFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setFilter({ ...filter, [name]: value });
+  };
+
+  const { tipoDestaque, stacks } = filter;
   return (
-    <section>
-      <form>
-        <div>
+    <section className="flex row justify section">
+      <form className="flex row justify form">
+        <div className="flex row form-div">
           <h2>Tipo de Destaque</h2>
-          <select>
+          <select className="select" name="tipoDestaque" value={tipoDestaque} onChange={handleFilter}>
             <option value="" disabled selected>Selecione uma opção</option>
             <option>teste</option>
           </select>
         </div>
-        <div>
+        <div className="flex row">
           <h2>Stacks</h2>
-          <select>
+          <select className="select" name="stacks" value={stacks} onChange={handleFilter}>
             <option value="" disabled selected>Selecione uma opção</option>
             <option>teste</option>
           </select>
         </div>
       </form>
-      <button type="button">Buscar</button>
-      <button type="button">Limpar filtros</button>
+      <div className="flex row div-buttons">
+        <button className="button" type="button">Buscar</button>
+        <button
+          className="button"
+          type="button"
+          onClick={() => setFilter({
+            tipoDestaque: '',
+            stacks: '',
+          })}
+        >
+          Limpar filtros
+        </button>
+      </div>
     </section>
   );
 }
--- app/src/page/Home.tsx
+++ app/src/page/Home.tsx
@@ -4,17 +4,9 @@ import Search from '../components/Search';
 import Header from '../components/Hearder';
 import data from '../persistence/PersistenceHelper';
 
-
 function Home(): JSX.Element {
   console.log(data);
   return (
-
-    <div>
-      <h1>Hello</h1>
-      <h1>World</h1>
-      
-    </div>
-
     <>
       <Header />
       <Search />
