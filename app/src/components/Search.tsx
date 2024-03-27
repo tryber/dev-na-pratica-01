3a14c05f228a53534c884af6beef4e8c4a8fb419
MauricioPassosF <mauricio.passos97@gmail.com>
2024-03-27 11:27:04 -0300

    Iniciando Search

---
+++ app/src/components/Search.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function Search(): JSX.Element {
+  const [filter, setFilter] = useState({
+    tipoDestaque: '',
+    stacks: '',
+  });
+  return (
+    <section>
+      <form>
+        <div>
+          <h2>Tipo de Destaque</h2>
+          <select>
+            <option value="" disabled selected>Selecione uma opção</option>
+            <option>teste</option>
+          </select>
+        </div>
+        <div>
+          <h2>Stacks</h2>
+          <select>
+            <option value="" disabled selected>Selecione uma opção</option>
+            <option>teste</option>
+          </select>
+        </div>
+      </form>
+      <button type="button">Buscar</button>
+      <button type="button">Limpar filtros</button>
+    </section>
+  );
+}
+
+export default Search;
--- app/src/page/Home.tsx
+++ app/src/page/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Search from '../components/Search';
 
 function Home(): JSX.Element {
   return (
     <div>
       <h1>Hello</h1>
       <h1>World</h1>
+      <Search />
     </div>
   );
 }
