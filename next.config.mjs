f01831c33ac7fa661e603426cdbc6bf4c6dfbd56
alanguilhermeM <alanguilherme13@outlook.com>
2024-03-28 06:40:13 -0300

    feat: nextjs.yml modify

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+    basePath: "/dev-na-pratica-01",
     output: "export",
-    distDir: "dist",
     images: {
         unoptimized: true,
     }
