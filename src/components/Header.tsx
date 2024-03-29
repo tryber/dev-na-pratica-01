b0ea1238a4e8064e49d40b920008f548ca94de29
alanguilhermeM <alanguilherme13@outlook.com>
2024-03-29 09:29:52 -0300

    feat: mergin between branches

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header: React.FC = () => {
   return (
       <header className="fixed w-full shadow-xl h-14 z-[100] bg-[#0C6D18] hover:bg-headerAndFooterBackground hover:text-white">
         <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-          <Image src="/logo-trybe.png" alt="Logo" width="100" height="5" />
+          <Image src="/logo-trybe.png" alt="Logo" width="100" height="5"  />
           <div>
             <ul className="hidden md:flex">
               <Link href="/">
