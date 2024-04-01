e91de339a45e847dc456563dd4ccbdca190c30c4
lu1z <luiz.alberto.laus.rosa@gmail.com>
2024-04-01 20:09:16 -0300

    home now renders filtered posts from context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,42 @@
+import { useContext, useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import DataContext from '../context/DataContext';
+import Highlight from '../components/Highlight';
+import Post from '../types/PostType';
 
 function Home() {
+  const [searchParams] = useSearchParams();
+  const data = useContext(DataContext);
+  const filtered = useMemo(
+    () => {
+      const searchFields = [...searchParams.entries()];
+      return searchFields.reduce<Post[]>(
+        (acc, [key, val]) => acc.filter(
+          ({ [key]: v }) => {
+            if (Array.isArray(v)) return v.includes(val);// tags
+            if (typeof v === 'object' && key in v) return v[key] === Number(val);// data
+            if (typeof v === 'number') return v === Number(val);// id
+            if (typeof v === 'string') return v.includes(val);// strings
+            return false;
+          },
+        ),
+        [...data],
+      );
+    },
+    [data, searchParams],
+  );
   return (
     <>
       <Header />
       <Search />
       <h2>Home</h2>
       <h1>Destrybers</h1>
+      <div>
+        <h1>Destaques do mês</h1>
+        {filtered.map((post) => <Highlight key={post.id} post={post} />)}
+      </div>
     </>
   );
 }
