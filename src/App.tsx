5b8b90ab6405d36f6f38314e5cfe0ce3e243f2f4
André Lucas <andrelucca99@gmail.com>
2024-04-01 08:51:44 -0300

    feat: add component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="bg-green-500 w-full h-screen">
-      <h1 className="text-center text-3xl text-white font-bold underline">
-        Hello world!
-      </h1>
+      <Header />
     </div>
   );
 }
