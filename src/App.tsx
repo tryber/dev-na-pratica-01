bedce76d1b3743418757e9137b24006c0c4fab6b
André Lucas <andrelucca99@gmail.com>
2024-04-01 10:12:47 -0300

    feat: add component card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css';
+import Card from './components/Card';
 import Header from './components/Header';
+
+import capa from './assets/medalha.png';
+import people from './assets/people.png';
 
 function App() {
   return (
     <div className="bg-green-500 w-full h-screen">
       <Header />
+      <Card
+        fotoCapa={ capa }
+        titulo="Teclado de Ouro"
+        descricao="Lorem Ipsum is simply dummy text
+        of the printing and typesetting industry.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        fotoUser={ people }
+        nome="Fulano da Silva"
+        stack="Front-end"
+        medalhas={ capa }
+      />
     </div>
   );
 }
