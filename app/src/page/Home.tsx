be5b009a845c8b999fbcf6145b211d4b95ccaf87
higo jhon <higojhon98@gmail.com>
2024-04-02 15:41:30 -0300

    Adiciona componente Info e estilos relacionados

--- app/src/page/Home.tsx
+++ app/src/page/Home.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Search from '../components/Search';
 import Header from '../components/Hearder';
 import data from '../persistence/PersistenceHelper';
+import Info from '../components/Info';
 
 function Home(): JSX.Element {
   console.log(data);
@@ -11,8 +12,7 @@ function Home(): JSX.Element {
       <Header />
       <Search />
       <div>
-        <h1>Hello</h1>
-        <h1>World</h1>
+        <Info />
       </div>
     </div>
   );
