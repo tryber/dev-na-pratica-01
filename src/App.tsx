c8d330b59ea45a4e40fc0e97abb95c2a68a1abff
lu1z <luiz.alberto.laus.rosa@gmail.com>
2024-04-03 18:11:22 -0300

    history functionality and changes on how home filter date

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import DataContext from './context/DataContext';
 import data from './persistence/PersistenceHelper';
 import Home from './pages/Home';
 import History from './pages/History';
 
 function App() {
+  const today = new Date();
+  const search = `date=${today.getMonth() + 1}-${today.getFullYear()}`;
+
   return (
     <DataContext.Provider value={data}>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to={{ pathname: '/home', search }} />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Routes>
     </DataContext.Provider>
