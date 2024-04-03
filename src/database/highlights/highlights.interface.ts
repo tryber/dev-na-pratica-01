80f029a15149f55129810848d80ca340159f342c
RenanFernandess <orenanfer@gmail.com>
2024-04-03 15:32:54 -0300

    feat: Change the format of tables and add essential stamps

--- src/database/highlights/highlights.interface.ts
+++ src/database/highlights/highlights.interface.ts
@@ -1,11 +1,13 @@
 import IStudent from '../students/student.interface';
+import ISeal from '../seals/seal.interface';
 
 interface IHighlight {
   id: number;
   title: string;
   description: string;
-  image: string;
+  banner: string;
   student: IStudent;
+  seal: ISeal;
 }
 
 export default IHighlight;
--- src/database/seals/seal.interface.ts
+++ src/database/seals/seal.interface.ts
@@ -1,5 +1,6 @@
 export default interface ISeal {
+  id: number;
   title: string;
   description: string;
-  image: string;
+  imageSeal: string;
 }
