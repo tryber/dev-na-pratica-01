31315da1b1779a6765bfc182ca339e76057e7874
higo jhon <higojhon98@gmail.com>
2024-04-04 15:51:52 -0300

    Atualizações de layout e remoção de código comentado

--- src/components/Highlight.tsx
+++ src/components/Highlight.tsx
@@ -5,25 +5,44 @@ type HighlightProps = {
 };
 
 function Highlight({ post }: HighlightProps) {
+  // const [isHovered, setIsHovered] = useState(false);
+
   const {
-    id, description, tags, name, picture, github, linkedin, title, illustration,
+    id, tags, name, picture, github, linkedin, illustration,
   } = post;
+
   return (
-    <div className="flex row div-highlight ">
-      <div className="flex column div-student">
-        <img src={picture} alt="Foto de perfil" />
-        <h3>{`Nome: ${name}`}</h3>
-        <a href={linkedin} target="_blank" rel="noreferrer">Linkedin</a>
-        <a href={github} target="_blank" rel="noreferrer">Github</a>
-      </div>
-      <div>
-        <h1>{title}</h1>
-        <p>{description}</p>
-        <img src={illustration} alt="Foto do destaque" />
-        <div>
-          {tags.map((tag) => <h4 key={`${id}-${tag}`}>{tag}</h4>)}
+    <div className="flex row div-highlight">
+      <div className="div-student">
+        <img className="img-person" src={picture} alt="Foto de perfil" />
+        <div className="info-person">
+          <p>{`Nome: ${name}`}</p>
+          <a href={linkedin} target="_blank" rel="noreferrer">Linkedin</a>
+          <a href={github} target="_blank" rel="noreferrer">Github</a>
         </div>
       </div>
+      <div className="div-flex">
+        <div className="img-port" style={{ backgroundImage: `url(${illustration})` }}>
+          <div className="overlay">
+            {tags.map((tag) => <h4 key={`${id}-${tag}`}>{tag}</h4>)}
+          </div>
+        </div>
+      </div>
+      {/* <div
+        className="projeto"
+        onMouseEnter={() => setIsHovered(false)}
+        onMouseLeave={() => setIsHovered(true)}
+      >
+        <img className="illustration" src={illustration} alt="Foto do destaque" />
+        {isHovered && (
+          <div className="overlay" />
+        )}
+        {isHovered && (
+          <div className="div-tag">
+            {tags.map((tag) => <h4 key={`${id}-${tag}`}>{tag}</h4>)}
+          </div>
+        )}
+      </div> */}
     </div>
   );
 }
