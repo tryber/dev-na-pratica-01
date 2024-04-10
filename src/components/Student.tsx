436f2fa9fa487f1718db145516c6e9661c7fe3c1
unknown <vitor_teixeira90@hotmail.com>
2024-04-10 16:51:57 -0300

    feat: mudar layout responsivo do header

--- src/components/Student.tsx
+++ src/components/Student.tsx
@@ -14,7 +14,7 @@ const Student: React.FC<StudentProps> = ({ image, student_user }) => {
   return (
     <div className="justify-center items-center md:mb-48 mb-32 mt-12 md:mt-32 md:w-full m-auto md:w-full w-[85%]">
       <div>
-        <div className="flex-col md:flex-row lg:ml-36 hidden md:flex">
+        <div className="flex-col md:flex-row lg:ml-12 hidden md:flex">
           <AboutPhrase name={student_user.name} phrase={student_user.phrase} about={student_user.about} />
           <ImageSvg image={image} linkedin={student_user.linkedin} />
         </div>
