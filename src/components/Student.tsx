ee962999ee41aa251a66c1e055b22c9ff3c5fc00
reinaldoper <reinaldoper83@gmail.com>
2024-04-10 13:59:50 -0400

    feat: adiciona component about

--- src/components/Student.tsx
+++ src/components/Student.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { StudentProps } from "@/types/StudentsProps";
 import ImageSvg from "./ImageSvg";
 import Svg from "./Svg";
+import AboutPhrase from "./AboutPhrase";
 
 // codigo foto direita
 
@@ -13,15 +14,7 @@ const StudentReverse: React.FC<StudentProps> = ({ image, student_user }) => {
       <div>
         <div className="flex flex-row lg:ml-36">
           <Svg linkedin={student_user.linkedin} />
-          <div className="text-white w-3/5 flex-col flex justify-start md:mr-8">
-            <h1 className="text-5xl "> {student_user.name}</h1>
-            <h3 className="text-base md:text-2xl lg:text-3xl mt-2">
-              {student_user.phrase}
-            </h3>
-            <h4 className="flex mt-4 justify-center items-center text-xs md:text-base ">
-              <i>{student_user.about}</i>
-            </h4>
-          </div>
+          <AboutPhrase name={student_user.name} phrase={student_user.phrase} about={student_user.about} />
           <ImageSvg image={image} />
         </div>
         <div className="mt-4">
--- src/components/StudentReverse.tsx
+++ src/components/StudentReverse.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { StudentProps } from "@/types/StudentsProps";
 import ImageSvg from "./ImageSvg";
 import Svg from "./Svg";
+import AboutPhrase from "./AboutPhrase";
 
 const StudentReverse: React.FC<StudentProps> = ({ image, student_user }) => {
   return (
@@ -12,15 +13,7 @@ const StudentReverse: React.FC<StudentProps> = ({ image, student_user }) => {
         <div className="flex flex-row lg:ml-48">
           <ImageSvg image={image} />
           <Svg linkedin={student_user.linkedin} />
-          <div className="text-white w-3/5 flex-col flex justify-start mr-8">
-            <h1 className="text-5xl "> {student_user.name}</h1>
-            <h3 className="text-base md:text-2xl lg:text-3xl mt-2">
-              {student_user.phrase}
-            </h3>
-            <h4 className="flex mt-4 justify-center items-center text-xs md:text-base ">
-              <i>{student_user.about}</i>
-            </h4>
-          </div>
+          <AboutPhrase name={student_user.name} phrase={student_user.phrase} about={student_user.about} />
         </div>
         <div className="mt-4 mx-8">
           <Card user={student_user} />
