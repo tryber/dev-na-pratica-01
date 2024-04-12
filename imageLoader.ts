7e8547a5df76a06146d92c713a5e03ac54e6002b
alanguilhermeM <alanguilherme13@outlook.com>
2024-04-12 09:42:00 -0300

    Image

--- imageLoader.ts
+++ imageLoader.ts
@@ -1,4 +1,6 @@
-const imageLoader = ({ src, width, quality }) => {
+import { ImageLoader } from 'next/image';
+
+const imageLoader: ImageLoader = ({ src, width, quality }) => {
   return `https://alanguilhermem.github.io/dev-na-pratica-02/${src}?w=${width}&q=${quality || 75}`;
 };
 
